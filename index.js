13382579f3ef007905265d7418c47a2547035294
Gabriel Burgos <gabrielburgosmm@gmail.com>
2022-06-29 16:47:40 -0700

    commit tree 6c0fa08ff54e2d4a1b88ebbaf2f9c8945d8755b5

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const { getModulesPluginNames } = require("@babel/preset-env");
+
 // Code your solutions in this file
+
+
+function writeCards(names,event) {
+    let filledCards = [];
+    for(let i = 0; i < names.length; i++)
+        filledCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    return filledCards;
+}
+
+function countDown( number ) {
+    while (  number > 0 ) {
+      console.log(  number );
+      number -= 1;
+    }
+    console.log(  number );
+  }
